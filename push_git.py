ba6763daac1314c9b1190f884232e33049af3cee
superyangbb@163.com <superyangbb@163.com>
2022-12-08 13:36:13 +0800

    just test baits

---
+++ push_git.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import subprocess
+import sys
+
+def main(token, git_url):
+    cmd = 'git init'
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'git add .'
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'git commit -m "just test baits"'
+    subprocess.call(cmd, shell=True)
+
+    
+    cmd = 'git config --global user.name "superyangbb@163.com"'
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'git config --global user.email "superyangbb@163.com"'
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'git remote remove origin'
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'git remote add origin https://{}@{}'.format(token, git_url)
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'git push -u origin master -f'
+    subprocess.call(cmd, shell=True)
+
+
+if __name__ == '__main__':
+    token = sys.argv[1]
+    git_url = sys.argv[2]
+    print(token, git_url)
+    main(token, git_url)
